e72d79e6f846459c7a0dfe1ffc6133d5aeabcb8a
Caia Chuang <caiachuang@gmail.com>
2021-05-12 13:14:44 -0700

    Serup GraphQL schema.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import GraphQLServer from 'graphql-yoga';
 
-const server = new GraphQLServer();
+const typeDefs = `
+    type Message {
+        id: ID!,
+        user: String!,
+        content: String!
+    }
+
+    type Query {
+        messages: [Message!]
+    }
+`
+
+const server = new GraphQLServer({ typeDefs });
 
 server.start(({port}) => {
     console.log(`Server on localhost:${port}`);
